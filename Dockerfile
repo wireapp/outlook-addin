69133925082836b0c4b43b4f2f3ed8e103e6ac25
Vedran Ivanković <33936733+Veki301@users.noreply.github.com>
2023-03-17 11:59:30 +0100

    docker compose for running client and api in tandem and redirecting /oauth2callback requests on client to api

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,5 @@ COPY --from=build /app/dist /usr/share/nginx/html
 COPY --from=build /certs /usr/share/certs
 RUN rm /etc/nginx/conf.d/default.conf
 COPY nginx/nginx.conf /etc/nginx/conf.d/
-EXPOSE 3000
+EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- server/server.js
+++ server/server.js
@@ -1,26 +1,34 @@
 const { default: axios } = require("axios");
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/oauth2callback", (req, res) => {
-    axios.post("https://staging-nginz-https.zinfra.io/oauth/token", {
+  axios
+    .post(
+      "https://staging-nginz-https.zinfra.io/oauth/token",
+      {
         grant_type: "authorization_code",
         client_id: "91ab148a-4b6c-4eac-aab6-60f316912f4d",
         client_secret: "92f7b4d4613f0e70ebf391a8f108e90c536bbccecb2b3eba59de32102a9831bf",
         code: req.query.code,
-        redirect_uri: "https://outlook.integrations.zinfra.io/oauth2callback"
-    }, {
+        redirect_uri: "https://outlook.integrations.zinfra.io/oauth2callback",
+      },
+      {
         headers: {
-            Accept: "application/json",
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    }).then((result) => {
-        console.log(result.data.access_token)
-        res.send("you are authorized " + result.data.access_token)
-    }).catch((err) => {
-        console.log(err);
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    )
+    .then((result) => {
+      console.log(result.data.access_token);
+      res.send("you are authorized " + result.data.access_token);
     })
-})
+    .catch((err) => {
+      console.log(err.response.data.message);
+      res.send("error " + err.response.data.message);
+    });
+});
