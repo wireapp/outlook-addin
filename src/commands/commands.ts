b0d5e9ea52a15a82e8cabf75c300ea342b844b4a
zeljkotutic <zeljko.tutic@gmail.com>
2023-03-28 14:16:46 +0200

    refactoring to authorize when needed

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -3,7 +3,10 @@
 
 //import { createGroupConversation, createGroupLink } from "../api/api";
 import { createEvent } from "../api/api";
+import { AuthResult } from "../api/types";
 import { appendToBody, getSubject, createMeetingLinkElement, getOrganizer, setLocation } from "../utils/mailbox";
+
+
 
 // Office is ready. Init
 Office.onReady(function () {
@@ -15,63 +18,99 @@ let mailboxItem;
 
 let pendingCreateConversation = false;
 
-export function addMeetingLink() {
-  let isLoggedIn: boolean = false;
-
-  let dialog;
-
-  pendingCreateConversation = true;
-
-  isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn"));
+async function addMeetingLink() {
+  let isLoggedIn = JSON.parse(localStorage.getItem("isLoggedIn"));
 
   if (!isLoggedIn) {
-    authorizeDialog();
-  } else {
-    if (pendingCreateConversation) {
-      createGroupConversationForCurrentMeeting();
-      pendingCreateConversation = false;
+    isLoggedIn = await authorizeDialog();
+  }
+
+  if (isLoggedIn) {
+    try {
+      await fetchWithAuthorizeDialog("/createGroupConversation", {
+        method: "POST",
+        body: JSON.stringify(mailboxItem),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    } catch (error) {
+      if (error.status === 401) {
+        localStorage.removeItem("isLoggedIn");
+        isLoggedIn = await authorizeDialog();
+        if (isLoggedIn) {
+          await fetchWithAuthorizeDialog("/createGroupConversation", {
+            method: "POST",
+            body: JSON.stringify(mailboxItem),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+        }
+      } else {
+        console.error(error);
+      }
     }
   }
 }
 
-function authorizeDialog() {
-  let isLoggedIn : boolean = false;
+async function fetchWithAuthorizeDialog(url: string, options: RequestInit): Promise<Response> {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      if (response.status === 401) {
+        const isLoggedIn = await authorizeDialog();
+        if (isLoggedIn) {
+          return await fetch(url, options);
+        } else {
+          throw new Error("Authorization failed");
+        }
+      } else {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+    } else {
+      return response;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
 
+function authorizeDialog(): Promise<boolean> {
   console.log("open dialog");
 
-  Office.context.ui.displayDialogAsync(
-    "https://outlook.integrations.zinfra.io/authorize",
-    { height: 70, width: 40 },
-    (asyncResult) => {
-      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.error("dialog result failed: " + asyncResult.error.message);
-      } else {
-        const dialog = asyncResult.value;
-        dialog.addEventHandler(
-          Office.EventType.DialogMessageReceived,
-          (messageEvent: Office.DialogParentMessageReceivedEventArgs) => {
-            console.log("DialogMessageReceived");
-            const authResult = JSON.parse(messageEvent.message) as any;
-            console.log("Auth result:", authResult);
+  return new Promise((resolve) => {
+    Office.context.ui.displayDialogAsync(
+      "https://outlook.integrations.zinfra.io/authorize",
+      { height: 70, width: 40 },
+      (asyncResult) => {
+        if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+          console.error("dialog result failed: " + asyncResult.error.message);
+          resolve(false);
+        } else {
+          const dialog = asyncResult.value;
+          dialog.addEventHandler(
+            Office.EventType.DialogMessageReceived,
+            (messageEvent: Office.DialogParentMessageReceivedEventArgs) => {
+              console.log("DialogMessageReceived");
+              const authResult = JSON.parse(messageEvent.message) as AuthResult;
+              console.log("Auth result:", authResult);
 
-            if(authResult.success) {
-              isLoggedIn = true;
-              localStorage.setItem("isLoggedIn", String(isLoggedIn));
+              if (authResult.success) {
+                localStorage.setItem("isLoggedIn", "true");
+                resolve(true);
+              } else {
+                resolve(false);
+              }
+
+              dialog.close();
             }
-
-            if (isLoggedIn && pendingCreateConversation) {
-              createGroupConversationForCurrentMeeting();
-              pendingCreateConversation = false;
-            }
-
-            dialog.close();
-          }
-        );
+          );
+        }
       }
-    }
-  );
-
-  return isLoggedIn;
+    );
+  });
 }
 
 function createGroupConversationForCurrentMeeting() {
