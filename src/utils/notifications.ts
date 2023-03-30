8444d67e81d40db1f48e829ff8c435c49299e824
zeljkotutic <zeljko.tutic@gmail.com>
2023-03-30 15:51:45 +0200

    remove notification after completion

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,4 +1,4 @@
-export function showNotification(notificationId, message, type, icon) {
+export function showNotification(key, message, type, icon) {
     const notificationMessage = {
       type,
       icon,
@@ -6,11 +6,21 @@ export function showNotification(notificationId, message, type, icon) {
       persistent: false,
     };
   
-    Office.context.mailbox.item.notificationMessages.addAsync(notificationId, notificationMessage, (result) => {
+    Office.context.mailbox.item.notificationMessages.addAsync(key, notificationMessage, (result) => {
       if (result.status === Office.AsyncResultStatus.Succeeded) {
-        console.log('Notification message added successfully.');
+        console.log(`Notification with key "${key}" added successfully.`);
       } else {
         console.error('Failed to add notification message:', result.error);
+      }
+    });
+}
+
+function removeNotification(key) {
+    Office.context.mailbox.item.notificationMessages.removeAsync(key, (result) => {
+      if (result.status === Office.AsyncResultStatus.Succeeded) {
+        console.log(`Notification with key "${key}" removed successfully.`);
+      } else {
+        console.error('Failed to remove notification:', result.error);
       }
     });
 }
