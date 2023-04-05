d3567c18f15d32e417d8cdcb7cfd4d55f5c47909
Zeljko Tutic <zeljko.tutic@gmail.com>
2023-04-05 18:33:17 +0200

    feat: convert add-in to client only version

---
+++ client/src/authorize/authorize.ts
@@ -0,0 +1,61 @@
+import * as CryptoJS from 'crypto-js';
+
+document.addEventListener('DOMContentLoaded', async function () {
+  await redirectToAuthorize();
+}, false);
+
+const redirectToAuthorize = async () => {
+  const clientId = '3af4a9c5-4ae3-42f9-a168-981bbca4c56f';
+  const redirectUri = 'https://outlook.integrations.zinfra.io/client/callback.html';
+  const responseType = 'code';
+  const state = await generateRandomState();
+  const scope = 'write:conversations write:conversations_code read:self read:feature_configs';
+
+  const codeChallengeMethod = 'S256';
+  const codeVerifier = await generateCodeVerifier();
+  const codeChallenge = await generateCodeChallenge(codeVerifier);
+  
+  sessionStorage.setItem('state', state);
+  sessionStorage.setItem('code_verifier', codeVerifier);
+
+  const url = new URL('https://wire-webapp-edge.zinfra.io/auth');
+  url.searchParams.append('client_id', clientId);
+  url.searchParams.append('redirect_uri', redirectUri);
+  url.searchParams.append('response_type', responseType);
+  url.searchParams.append('state', state);
+  url.searchParams.append('scope', scope);
+  url.searchParams.append('code_challenge_method', codeChallengeMethod);
+  url.searchParams.append('code_challenge', codeChallenge);
+  url.hash = 'authorize';
+
+  window.location.href = url.href;
+};
+
+const generateRandomState = async (): Promise<string> => {
+  return generateRandomHexString(16);
+};
+
+const generateCodeVerifier = async (): Promise<string> => {
+  return generateRandomHexString(64);
+};
+
+const generateCodeChallenge = async (codeVerifier: string): Promise<string> => {
+  const hash = CryptoJS.SHA256(codeVerifier);
+  const base64Url = hash
+    .toString(CryptoJS.enc.Base64)
+    .replace('+', '-')
+    .replace('/', '_')
+    .replace(/=+$/, '');
+
+  return base64Url;
+};
+
+const generateRandomHexString = (length) => {
+  const dec2hex = (dec) => {
+    return dec.toString(16).padStart(2, "0");
+  };
+
+  const arr = new Uint8Array(Math.ceil(length / 2));
+  window.crypto.getRandomValues(arr);
+  return Array.from(arr, dec2hex).join('').slice(0, length);
+};
---
+++ client/src/callback/callback.ts
@@ -0,0 +1,79 @@
+/* global global, Office, self, window */
+import { AuthResult } from '../types/types'
+
+document.addEventListener('DOMContentLoaded', async function () {
+  console.log('BEFORE handling callback');
+  await handleCallback();
+}, false);
+
+const handleCallback = async (): Promise<void> => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const code = urlParams.get('code');
+  const receivedState = urlParams.get('state');
+  const storedCodeVerifier = sessionStorage.getItem('code_verifier');
+  const storedState = sessionStorage.getItem('state');
+
+  console.log('handleCallback');
+  console.log('code: ', code);
+  console.log('receivedState: ', receivedState);
+  console.log('storedCodeVerifier: ', storedCodeVerifier);
+  console.log('storedState: ', storedState);
+  if (code && receivedState && storedCodeVerifier) {
+    if (receivedState !== storedState) {
+      console.error('State validation failed');
+      return;
+    }
+
+    try {
+      const authResult = await exchangeCodeForTokens(code, storedCodeVerifier);
+
+      console.log('authResult: ', authResult);
+        
+      Office.onReady(() => {
+        Office.context.ui.messageParent(JSON.stringify(authResult));
+      });
+    } catch (error) {
+      console.error('Error during token exchange:', error);
+    }
+  }
+};
+
+const exchangeCodeForTokens = async (code: string, codeVerifier: string): Promise<AuthResult> => {
+  const clientId = '3af4a9c5-4ae3-42f9-a168-981bbca4c56f';
+  const redirectUri = 'https://outlook.integrations.zinfra.io/client/callback.html';
+  const tokenEndpoint = 'https://staging-nginz-https.zinfra.io/oauth/token';
+
+  const body = new URLSearchParams();
+  body.append('grant_type', 'authorization_code');
+  body.append('client_id', clientId);
+  body.append('code', code);
+  body.append('redirect_uri', redirectUri);
+  body.append('code_verifier', codeVerifier);
+
+  const response = await fetch(tokenEndpoint, {
+    method: 'POST',
+    headers: {
+      Accept: "application/json",
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: body.toString(),
+  });
+
+  console.log('body.toString(): ', body.toString());
+
+  console.log('response: ', response);
+
+  if (response.ok) {
+    const json = await response.json();
+    const { access_token, refresh_token } = json;
+
+    console.log('access_token: ', access_token);
+    console.log('refresh_token: ', refresh_token);
+
+    return { success: true, access_token, refresh_token};
+  } else {
+    throw new Error('Failed to exchange authorization code for tokens');
+  }
+};
+
+
