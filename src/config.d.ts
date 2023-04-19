cb3e4ade1537277138ec4733d861fd3e678494ff
Zeljko Tutic <zeljko.tutic@gmail.com>
2023-04-19 21:13:22 +0200

    updates to prevent config.js rename

--- src/config.d.ts
+++ src/config.d.ts
@@ -1,8 +1,10 @@
-declare const config: {
+interface Config {
   addInBaseUrl: string;
   apiBaseUrl: string;
   authorizeUrl: string;
   clientId: string;
-};
+}
+
+declare const config: Config;
 
 export default config;
