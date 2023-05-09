85d1f2f55d2fc851f4c47a4eb50560ebc1af77f1
Florian Klink <flokli@flokli.de>
2023-05-09 15:00:17 +0300

    nginx.conf: drop ADDIN_HOST substitution
    Use the `listen 80 default_server;` directive to match on all hostnames.
    
    This will cause nginx to reply to all requests, no matter which `Host`
    is used in the request header.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,5 @@
 
 envsubst < /usr/share/nginx/html/manifest.xml.template > /usr/share/nginx/html/manifest.xml
 envsubst < /usr/share/nginx/html/config.js.template > /usr/share/nginx/html/config.js
-envsubst < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
