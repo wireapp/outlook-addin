a45830f0cd0b8c3ca453acd91a37bc6189dcb90e
zeljkotutic <zeljko.tutic@gmail.com>
2023-05-10 11:13:37 +0200

    Replace env variables from hosts to urls

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # envsubst coming with the nginx container unfortunately doesn't support
 # -no-unset and -no-empty parameters, so make sure to keep this list in sync!
 
-if [ -z ${ADDIN_HOST:-} ]; then echo "ADDIN_HOST is unset"; exit 1; fi
-if [ -z ${API_HOST:-} ]; then echo "API_HOST is unset"; exit 1; fi
-if [ -z ${AUTHORIZE_HOST:-} ]; then echo "AUTHORIZE_HOST is unset"; exit 1; fi
+if [ -z ${BASE_URL:-} ]; then echo "BASE_URL is unset"; exit 1; fi
+if [ -z ${WIRE_API_BASE_URL:-} ]; then echo "WIRE_API_BASE_URL is unset"; exit 1; fi
+if [ -z ${WIRE_API_AUTHORIZATION_ENDPOINT:-} ]; then echo "WIRE_API_AUTHORIZATION_ENDPOINT is unset"; exit 1; fi
 if [ -z ${CLIENT_ID:-} ]; then echo "CLIENT_ID is unset"; exit 1; fi
 
 envsubst  < /usr/share/nginx/html/manifest.xml.template > /usr/share/nginx/html/manifest.xml
