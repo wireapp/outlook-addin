ff75d43fbebddd09610001ede98391bfb99473de
MarianKijewski <mariankijewski@outlook.com>
2026-09-11 11:18:00 +0200

    Upgrade react{,-dom} version from 17 to 18
    Use native Fast Refresh instead of deprecated react-hot-loader
    
    * Install the requried dependencies
    * Update webpack and babel configs
    * Enable HotModuleReplacement
    * Remove all the occurences of the old plugin
    
    Update client rendering API
    
    Only now React 18 is actually used.
    Reference: https://reactjs.org/link/switch-to-createroot
    
    Migrate from Fluent UI React v8 to v9
    
    * `ThemeProvider`: https://storybooks.fluentui.dev/react/?path=/docs/concepts-migration-keeping-design-consistent--docs
    * Button migration: https://storybooks.fluentui.dev/react/?path=/docs/concepts-migration-from-v8-components-button-migration--docs
    * Spinner migration: https://storybooks.fluentui.dev/react/?path=/docs/concepts-migration-from-v8-components-spinner-migration--docs
    * Remove dead code for icons initialization
    
    Remove IE11 from browserlist

--- src/taskpane/components/LoggedIn.tsx
+++ src/taskpane/components/LoggedIn.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { DefaultButton } from "@fluentui/react";
+import { Button } from "@fluentui/react-components";
 import type { SelfUser } from "../../types/SelfUser";
 
 interface LoggedInProps {
@@ -15,7 +15,7 @@ const LoggedIn: React.FC<LoggedInProps> = ({ user, onLogout }) => (
       Username: {user.handle}<br/>
       E-mail: {user.email}<br/>
       <br/>
-      <DefaultButton onClick={onLogout}>Disconnect Add-in</DefaultButton>
+      <Button onClick={onLogout}>Disconnect Add-in</Button>
   </div>
 );
 
